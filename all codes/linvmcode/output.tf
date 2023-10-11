93f8976af492e78b3b43915f13d9574f4106778a
Vinaydevops12 <142679137+Vinaydevops12@users.noreply.github.com>
2023-10-11 23:26:04 +0530

    repocipl folder added and all code file deleted

--- all codes/linvmcode/output.tf
+++
@@ -1,5 +0,0 @@
-output "vm_public_ip" {
-  value = [
-    for virtual_machine in azurerm_public_ip.pip : virtual_machine.ip_address
-  ]
-}
--- all codes/rgcode/output.tf
+++
@@ -1,5 +0,0 @@
-output "rg" {
-  value = [
-    for rg in azurerm_resource_group.example : rg.id
-  ]
-}
